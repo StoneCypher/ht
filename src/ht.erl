4fae6963f992224f22c51ecbe096ff757375dc89
John Haugeland <stonecypher@gmail.com>
2014-07-11 23:52:40 -0700

    specs

--- src/ht.erl
+++ src/ht.erl
@@ -24,6 +24,8 @@
 
 
 
+-spec stub(Config :: function() | record() | list()) -> pid() | { error, any() }.
+
 stub(Proplist) when is_list(Proplist) ->
 
     stub(#ht_options{  });
@@ -41,5 +43,15 @@ stub(Proplist) when is_record(Proplist, ht_options) ->
 
 
 stub(Handler) when is_function(Handler) ->
+
+    stub(#ht_options{  }).
+
+
+
+
+
+-spec stub(Handler :: function(), Post :: integer()) -> pid() | { error, any() }.
+
+stub(Handler, Port) when is_function(Handler), is_integer(Port), Port >= 0 ->
 
     stub(#ht_options{  }).
